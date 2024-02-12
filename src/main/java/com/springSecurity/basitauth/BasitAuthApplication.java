bb54f806f8a888dc54571697cb6a86093bd29ab4
GorkemBedel <117198668+GorkemBedel@users.noreply.github.com>
2024-02-12 20:55:25 +0300

    first commit

---
+++ src/main/java/com/springSecurity/basitauth/BasitAuthApplication.java
@@ -0,0 +1,13 @@
+package com.springSecurity.basitauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasitAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasitAuthApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springSecurity/basitauth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.springSecurity.basitauth.repository;
+
+import com.springSecurity.basitauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String userName);
+}
